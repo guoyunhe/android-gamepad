0c5d805f8990e601bec72a0da9743f79f753fc98
Guo Yunhe <i@guoyunhe.me>
2025-08-03 18:43:19 +0800

    feat: nav

--- src/layouts/landing/TopNav.tsx
+++ src/layouts/landing/TopNav.tsx
@@ -1,16 +1,10 @@
-import { AuthStatus, useAuthStatus, useAuthUser, useLogout } from '@guoyunhe/react-auth';
 import {
-  AutoAwesome as AutoAwesomeIcon,
-  CreditCard,
-  Dashboard as DashboardIcon,
-  Login as LoginIcon,
-  Logout as LogoutIcon,
+  Android as AndroidIcon,
+  GitHub as GitHubIcon,
   Menu as MenuIcon,
-  PersonAdd as PersonAddIcon,
-  Settings as SettingsIcon,
-  SupportAgent as SupportAgentIcon,
+  SportsEsports as SportsEsportsIcon,
 } from '@mui/icons-material';
-import { AppBar, Avatar, Box, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import { ThemeToggle } from 'material-app';
 import { useTranslation } from 'react-i18next';
 import { Link, useLocation } from 'wouter';
@@ -23,9 +17,6 @@ export interface TopNavProps {
 export default function TopNav({ onMenuButtonClick }: TopNavProps) {
   const { t } = useTranslation();
   const [location] = useLocation();
-  const [status] = useAuthStatus();
-  const [user] = useAuthUser();
-  const logout = useLogout();
 
   return (
     <AppBar position="static" color="transparent" elevation={0}>
@@ -45,13 +36,13 @@ export default function TopNav({ onMenuButtonClick }: TopNavProps) {
         >
           <Box
             component="img"
-            src="/logo.svg"
+            src="/logo.png"
             width={32}
             height={32}
             sx={{ mr: 1, display: { xs: 'none', sm: 'block' } }}
           />
           <Typography fontSize={20} color="inherit" component="div">
-            {import.meta.env.VITE_APP_NAME}
+            Android Gamepad
           </Typography>
         </Box>
         <Stack direction="row" ml={3} sx={{ display: { xs: 'none', sm: 'flex' } }}>
@@ -60,109 +51,37 @@ export default function TopNav({ onMenuButtonClick }: TopNavProps) {
             color="inherit"
             disableElevation
             component={Link}
-            to="/features"
-            startIcon={<AutoAwesomeIcon />}
+            to="/"
+            startIcon={<AndroidIcon />}
           >
-            {t('Features')}
+            {t('Games')}
           </Button>
           <Button
             variant={location === '/pricing' ? 'contained' : 'text'}
             color="inherit"
             disableElevation
-            startIcon={<CreditCard />}
+            startIcon={<SportsEsportsIcon />}
             component={Link}
-            to="/pricing"
+            to="/controllers"
           >
-            {t('Pricing')}
+            {t('Controllers')}
           </Button>
           <Button
             variant={location === '/support' ? 'contained' : 'text'}
             color="inherit"
             disableElevation
-            startIcon={<SupportAgentIcon />}
-            component={Link}
-            to="/support"
+            startIcon={<GitHubIcon />}
+            component="a"
+            href="https://github.com/guoyunhe/android-gamepad"
           >
-            {t('Support')}
+            GitHub
           </Button>
         </Stack>
         <Box flex="1 1 auto" />
         <ThemeToggle />
-        {status === AuthStatus.LoggedIn && user ? (
-          <Stack direction="row">
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={
-                <Avatar src={user.avatar?.url} alt={user.name} sx={{ width: 24, height: 24 }} />
-              }
-              component={Link}
-              to="/app/settings"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {user.name}
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={<DashboardIcon />}
-              component={Link}
-              to="/app"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {t('Dashboard')}
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={<SettingsIcon />}
-              component={Link}
-              to="/app"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {t('Settings')}
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={<LogoutIcon />}
-              onClick={logout.submit}
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {t('Logout')}
-            </Button>
-          </Stack>
-        ) : (
-          <Stack direction="row">
-            <LanguageMenu />
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={<LoginIcon />}
-              component={Link}
-              to="/login"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {t('Login')}
-            </Button>
-            <Button
-              variant="text"
-              color="inherit"
-              disableElevation
-              startIcon={<PersonAddIcon />}
-              component={Link}
-              to="/register"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              {t('Register')}
-            </Button>
-          </Stack>
-        )}
+        <Stack direction="row">
+          <LanguageMenu />
+        </Stack>
       </Toolbar>
     </AppBar>
   );
