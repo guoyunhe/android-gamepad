481280c779f737cc72c37ecae19b71948a295c29
Guo Yunhe <i@guoyunhe.me>
2025-08-06 23:34:06 +0800

    fix: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,15 +4,8 @@ const date = new Date();
 
 export default function Footer() {
   return (
-    <Box
-      component="footer"
-      p={3}
-      display="flex"
-      textAlign="center"
-      fontSize="14px"
-      color={colors.grey[700]}
-    >
-      <Box>&copy; {date.getFullYear()} Guo Yunhe</Box>
+    <Box component="footer" p={3} textAlign="center" fontSize="14px" color={colors.grey[700]}>
+      &copy; {date.getFullYear()} Guo Yunhe
     </Box>
   );
 }
