25d85e392f41533e32504c6eef21cd2f0d73d75a
Guo Yunhe <i@guoyunhe.me>
2025-08-09 15:36:28 +0800

    chore: flycast

--- scripts/sort-games.ts
+++ scripts/sort-games.ts
@@ -7,4 +7,18 @@ const games: Game[] = JSON.parse(await readFile(gamesFile, 'utf-8'));
 
 games.sort((a, b) => a.play.localeCompare(b.play));
 
+function trimGooglePlayImage(url: string) {
+  if (url.includes('=') || url.includes('/')) {
+    return url.substring(url.lastIndexOf('/') + 1, url.indexOf('='));
+  }
+  return url;
+}
+
+games.forEach((game) => {
+  if (game.icon) {
+    game.icon = trimGooglePlayImage(game.icon);
+  }
+  game.screenshots = game.screenshots?.map(trimGooglePlayImage);
+});
+
 await writeFile(gamesFile, JSON.stringify(games, null, 2));
